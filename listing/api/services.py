96152b6f8b9015c6ae7d90cf65fa1f797ea1b438
Matheus <matheus_vilela@icloud.com>
2021-08-10 21:09:11 -0300

    create services

--- listing/api/services.py
+++ listing/api/services.py
@@ -1,15 +1,3 @@
 from django.contrib import auth
-from . import models
+from .import models
 
-def user_login(request, username: str, password: str) -> models.User:
-
-    user = auth.authenticate(request, username=username, password=password)
-    if not user:
-        return None
-
-    auth.login(request, user)
-    return user
-
-
-def user_logout(request):
-    return auth.logout(request)
